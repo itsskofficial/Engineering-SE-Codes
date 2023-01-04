c3f3b6c0a6ba906b634678fdf75a3ce2f190cfa3
SK <sarthakkarandikar03@gmail.com>
2023-01-04 16:35:52 +0000

    Jan 4, 2023, 10:05 PM

--- FDS/list.py
+++ FDS/list.py
@@ -40,7 +40,7 @@ def crick_bad(cricket,badminton): #students who play both cricket and badminton
 
 def crick_or_bad(cricket,badminton,crickbad): #students who play cricket and badminton but not both
   crickorbad=cricket+badminton #concatenate both lists
-  for i in crick_or_bad: #check in intersection of set
+  for i in crickorbad: #check in intersection of set
     for j in crickbad:
       if i==j:
         crickorbad.remove(i) #if found remove from the list
