788939d25d32ce70514326ca2786b8a96182b2d8
SK <sarthakkarandikar03@gmail.com>
2023-01-27 09:57:31 +0000

    Jan 27, 2023, 3:27 PM

--- FDS/radix.py
+++ FDS/radix.py
@@ -25,9 +25,9 @@ def radix_sort(arr,n):
             for i in range(n-1):
                 for j in range(0,n-i-1):
                     if arr[j]>arr[j+1]:
-                    temp=arr[j]
-                    arr[j]=arr[j+1]
-                    arr[j+1]=temp
+                        temp=arr[j]
+                        arr[j]=arr[j+1]
+                        arr[j+1]=temp
 
 
     arr=[]
