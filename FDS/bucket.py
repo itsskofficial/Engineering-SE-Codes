5b99016dc5b908a6ae61eef49204a749734cca96
SK <sarthakkarandikar03@gmail.com>
2023-01-27 10:52:55 +0000

    Jan 27, 2023, 4:22 PM

---
+++ FDS/bucket.py
@@ -0,0 +1,9 @@
+arr = []
+n = int(input("Enter no of elements in the array: "))
+print("\nEnter the elements one by one now: ")
+for i in range(n):
+    ele = int(input())
+    arr.append(ele)
+print(f"You entered the array : {arr}")
+
+def bucket
