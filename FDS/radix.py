3b69d7e85d04b5156498015ce0ec00f0d4e698d9
SK <sarthakkarandikar03@gmail.com>
2023-01-27 08:36:53 +0000

    Jan 27, 2023, 2:06 PM

--- FDS/radix.py
+++ FDS/radix.py
@@ -7,7 +7,7 @@ for i in range(n):
 print(f"You entered the array : {arr}")
 
 def radix_sort(arr,n):
-    radix=[0] 
+    radix=[[0]*9 ]
     max_ele=max(arr)
     highest_place=len(str(max_ele))
     for i in range(1,highest_place+1):
