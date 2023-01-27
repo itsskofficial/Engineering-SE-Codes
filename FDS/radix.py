e69f9a39d0bdaf2b11d5db32e67abb3be52149da
SK <sarthakkarandikar03@gmail.com>
2023-01-27 08:28:07 +0000

    Jan 27, 2023, 1:58 PM

--- FDS/radix.py
+++ FDS/radix.py
@@ -13,4 +13,5 @@ def radix_sort(arr,n):
     for i in range(1,highest_place+1):
         for j in arr:
             rem=j%(10**i)
-            radix[rem].append()
+            radix[rem].append(j)
+    
