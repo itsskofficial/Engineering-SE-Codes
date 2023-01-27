e68b7caffa7e4cab5d9905957e97667d18e6aebe
SK <sarthakkarandikar03@gmail.com>
2023-01-27 10:20:03 +0000

    Jan 27, 2023, 3:50 PM

--- FDS/counting.py
+++ FDS/counting.py
@@ -9,10 +9,10 @@ print(f"You entered the array : {arr}")
 def count_sort(arr,n):
     min_ele=min(arr)
     max_ele=max(arr)
-    temp=[]
+    tem1=[]
     count=[]
     for i in range(min_ele,max_ele+1):
-        temp.append(i)
+        temp1.append(i)
     for i in arr:
         c=0
         for j in arr:
