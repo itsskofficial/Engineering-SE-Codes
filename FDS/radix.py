308769f52df0e17ca33086ca5f9342babd8468dd
SK <sarthakkarandikar03@gmail.com>
2023-01-27 08:18:23 +0000

    Jan 27, 2023, 1:48 PM

--- FDS/radix.py
+++ FDS/radix.py
@@ -1,4 +1,7 @@
+arr=[]
 n=int(input("Enter no of elements in the array: "))
+print("\nEnter the elements one by one now: ")
 for i in range(n):
     ele=int(input())
     arr.append(ele)
+print(f"You entered the array : {arr}")
