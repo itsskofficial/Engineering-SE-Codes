3ecf58bcb33be0791a15dc89282609cdc09da6c6
SK <sarthakkarandikar03@gmail.com>
2023-02-06 06:15:50 +0000

    Feb 6, 2023, 11:45 AM

--- FDS/pqueue.cpp
+++ FDS/pqueue.cpp
@@ -19,7 +19,8 @@ void Queue::enqueue(){
     cout << "Enter no to insert in Queue: ";
     cin>>temp->data;
     if (rear==NULL){
-        front=rear=
+        front=temp;
+        rear = temp;
     }
 }
 
