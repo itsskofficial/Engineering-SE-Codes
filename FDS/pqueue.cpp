b3e638cc150b2c0bb241bed685ecb721d5786a51
SK <sarthakkarandikar03@gmail.com>
2023-02-06 06:26:40 +0000

    Feb 6, 2023, 11:56 AM

---
+++ FDS/pqueue.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node *next;
+};
+class Queue{
+    public:
+        node *front=NULL;
+        node *rear=NULL;
+        void enqueue();
+        void dequeue();
+        void display();
+};
+
+void Queue::enqueue(){
+    node *temp;
+    temp=new node;
+    cout << "Enter no to insert in Queue: ";
+    cin>>temp->data;
+    if (rear==NULL){
+        front=temp;
+        rear = temp;
+        return;
+    }
+
+    rear->next=temp;
+    rear = temp;
+}
+
+void Queue::dequeue(){
+    if (front==NULL){
+        return;
+    }
+
+    node *temp=front;
+    front=front->next;
+
+    if (front==NULL){
+        rear = NULL;
+    }
+
+    delete temp;
+}
+
+void Queue::display(){
+    node *temp=front;
+    while(temp!=rear){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<temp->data;
+}
